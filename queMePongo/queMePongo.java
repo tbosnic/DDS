7b6bd2632ecd6c7e52d390da1470390dca2d297d
tbosnic <tomasbosnic@gmail.com>
2022-04-29 18:15:51 -0300

    QMP segunda iteracion

--- queMePongo/queMePongo.java
+++ queMePongo/queMePongo.java
@@ -1,17 +1,54 @@
+class Borrador{
+    TipoPrenda tipoPrenda;
+    Material material;
+    Color colorPrincipal;
+    Color colorSecundario;
+    Trama trama = Trama.LISA;
+
+    Borrador(TipoPrenda tipoPrenda){
+
+        // VALIDAR QUE NO SEA NULO
+
+        this.tipoPrenda = tipoPrenda;
+    }
+
+    public void especificarColorPrincipal(Color color){
+        this.colorPrincipal = color;
+    }
+
+    public void especificarColorSecundario(Color color){
+        this.colorSecundario = color;
+    }
+
+    public void especificarMaterial(Material material){
+        this.material = material;
+    }
+
+    public void especificarTrama(Trama trama){
+        this.trama = trama;
+    }
+
+    public Prenda crearPrenda(){
+
+        // VALIDAR QUE NINGUN ATRIBUTO SEA NULL (menos color secundario), CASO CONTARIO, LANZAR EXCEPCION
+
+        return new Prenda(tipoPrenda, material, colorPrincipal, colorSecundario, trama);
+    }
+}
+
 class Prenda{
     TipoPrenda tipoPrenda;
     Material material;
     Color colorPrincipal;
     Color colorSecundario;
-
-    Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal, Color colorSecundario){
-
-        //VALIDAR QUE SE PASAN TODOS LOS PARAMETROS (no validar el color secundario), CASO CONTARIO, LANZAR EXCEPCION
-
+    Trama trama;
+    
+    Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal, Color colorSecundario, Trama trama){
         this.tipoPrenda = tipoPrenda;
         this.material = material;
         this.colorPrincipal = colorPrincipal;
         this.colorSecundario = colorSecundario;
+        this.trama = trama;
     }
 
     Categoria categoria(){
@@ -26,7 +63,6 @@ enum Material{
     LAYCRA,
     CUERO
 }
-
 class Color{
     int rojo, azul, verde;
 
@@ -37,68 +73,19 @@ class Color{
     }
 }
 
-abstract class TipoPrenda{
+class TipoPrenda{
+
     Categoria categoria;
 
     TipoPrenda(Categoria categoria){
-        if(!this.esCategoriaValida(categoria)){
-            // LANZAR EXCEPCION
-        }
+
+        // VALIDAR QUE LA CATEGORIA ES VALIDA PARA EL TIPO DE PRENDA, CASO CONTARIO, LANZAR EXCEPCION
+
         this.categoria = categoria;
     }
 
-    Categoria getCategoria(){
+    public Categoria getCategoria(){
         return categoria;
-    }
-
-    abstract boolean esCategoriaValida(Categoria categoria);
-}
-
-class Zapatos extends TipoPrenda{
-
-    Zapatos(Categoria categoria){
-        super(categoria);
-    }
-
-    boolean esCategoriaValida(Categoria categoria){
-        if (categoria != Categoria.CALZADO){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-}
-
-class CamisaMangasCortas extends TipoPrenda{
-
-    CamisaMangasCortas(Categoria categoria){
-        super(categoria);
-    }
-
-    boolean esCategoriaValida(Categoria categoria){
-        if (categoria != Categoria.PARTE_SUPERIOR){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-}
-
-class Pantalon extends TipoPrenda{
-
-    Pantalon(Categoria categoria){
-        super(categoria);
-    }
-
-    boolean esCategoriaValida(Categoria categoria){
-        if (categoria != Categoria.PARTE_INFERIOR){
-            return false;
-        }
-        else{
-            return true;
-        }
     }
 }
 
@@ -107,4 +94,12 @@ enum Categoria{
     CALZADO, 
     PARTE_INFERIOR, 
     ACCESORIOS;
+}
+
+enum Trama{
+    LISA,
+    RAYADA,
+    LUNARES,
+    CUADROS,
+    ESTAMPADO;
 }
