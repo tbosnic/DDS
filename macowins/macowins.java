6d9374e862fbc2b0be7b26e5cf4ac0537a049a29
tbosnic <tomasbosnic@gmail.com>
2022-05-02 20:15:41 -0300

    actualizando diagrama de clases y codigo

--- macowins/macowins.java
+++ macowins/macowins.java
@@ -1,82 +1,154 @@
-public class Prenda {
-    private int precioPropio;
-    private TipoPrenda tipo;
-    private EstadoPrenda estado;
-    
-    public int getPrecioVenta(){
-        return estado.calcularPrecioVenta(precioPropio);
-    }
-    
-    public TipoPrenda getTipoPrenda(){
-        return tipo;
-    }
-}
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
-public enum TipoPrenda {
-	SACO, PANTALON, CAMISA;
-}
-
-public interface EstadoPrenda {
-	public int calcularPrecioVenta(int precioPropio);
-}
-
-public class Nueva implements EstadoPrenda {
-	public int calcularPrecioVenta(int precioPropio){
-		return precioPropio;
+class Negocio {
+	private List<Venta> ventas;
+  
+	public Negocio(){
+	  ventas = new ArrayList<>();
+	}
+  
+	public void registrarVenta(Venta venta){
+	  ventas.add(venta);
+	}
+  
+	public Double gananciasDeUnDia(LocalDate unDia){
+	  return ventasDeUnDia(unDia).stream().mapToDouble(venta -> venta.importe()).sum();
+	}
+  
+	private List<Venta> ventasDeUnDia(LocalDate unDia){
+	  return ventas.stream().filter(venta -> venta.esDeFecha(unDia)).collect(Collectors.toList());
 	}
 }
 
-public class Promocion implements EstadoPrenda {
-	int valor;
-	
-	public int calcularPrecioVenta(int precioPropio){
-		return precioPropio - valor;
+// PATRON TEMPLATE METHOD -> CLASE PADRE, DONDE HAY METODOS CON COMPORTAMIENTO COMUN EN SUS CLASES HIJAS, EXCEPTO POR UNA PEQUENIA PARTE, DONDE CADA CLASE HIJA LO IMPLEMENTA
+// 							 A SU MANERA.
+
+abstract class Venta {
+	private List<Item> items;
+	private LocalDate fecha;
+  
+	public Venta(List<Item> items, LocalDate fecha){
+	  this.items = items;
+	  this.fecha = fecha;
+	}
+  
+	public boolean esDeFecha(LocalDate fecha){
+	  return (this.fecha.isEqual(fecha));
+	}
+  
+	public Double importe(){
+	  return recargo(this.sumaDeItems());
+	}
+  
+	private Double sumaDeItems(){
+	  return items.stream().mapToDouble(item -> item.importe()).sum();
+	}
+  
+	protected abstract Double recargo(Double importeBase);
+}
+
+class VentaEnEfectivo extends Venta{
+	public VentaEnEfectivo(List<Item> items, LocalDate fecha){
+	  super(items, fecha);
+	}
+
+	@Override
+	protected Double recargo(Double importeBase) {
+	  return importeBase;
 	}
 }
 
-public class Liquidacion implements EstadoPrenda {
-	public int calcularPrecioVenta(int precioPropio){
-		return precioPropio * 0.50;
+class VentaConTarjeta extends Venta{
+	Integer cantidadCuotas;
+	Double coeficienteTarjeta;
+  
+	public VentaConTarjeta(List<Item> items, LocalDate fecha, Integer cantidadCuotas, Double coeficienteTarjeta){
+	  super(items, fecha);
+	  this.cantidadCuotas = cantidadCuotas;
+	  this.coeficienteTarjeta = coeficienteTarjeta;
+	}
+
+	@Override
+	protected Double recargo(Double importeBase) {
+	  return importeBase + (importeBase * 0.01 + cantidadCuotas * coeficienteTarjeta);
 	}
 }
 
-public class Negocio {
-	private List<Venta> ventas = new ArrayList<>();
-	
-	public int calcularGananciasDeDia(LocalDate dia){
-		return ventas.stream().filter(venta -> venta.getDia().equals(dia)).sum(venta -> venta.PrecioVentaTotal());
+class Item {
+	private Prenda prenda;
+	private Integer cantidad;
+  
+	public Item(Prenda prenda, Integer cantidad){
+	  this.prenda = prenda;
+	  this.cantidad = cantidad;
+	}
+  
+	public Double importe(){
+	  return prenda.precioVenta() * cantidad;
 	}
 }
 
-public class Venta {
-	private List<Prenda> prendasVendidas = new ArrayList<>();
-	private LocalDate dia;
-	private MedioDePago medio;
-	
-	public int PrecioVentaNeto(){
-		return prendasVendidas.sum(prenda -> prenda.getPrecioVenta());
+class Prenda {
+	private Integer precioOriginal;
+	private TipoPrenda tipoPrenda;
+	private EstadoPrenda estadoPrenda;
+  
+	public Prenda(Integer precioOriginal, TipoPrenda tipoPrenda, EstadoPrenda estadoPrenda){
+	  this.precioOriginal = precioOriginal;
+	  this.tipoPrenda = tipoPrenda;
+	  this.estadoPrenda = estadoPrenda;
 	}
-	
-	public int PrecioVentaTotal(){
-		return medio.calcularPrecioTotal(getPrecioVentaNeto());
+  
+	public Double precioVenta(){
+	  return estadoPrenda.precioFinal(this.precioOriginal);
 	}
-}
-
-public interface MedioDePago {
-	public int calcularPrecioTotal(int precio);
-}
-
-public class Efectivo implements MedioDePago {
-	public int calcularPrecioTotal(int precio){
-		return precio;
+  
+	public TipoPrenda tipoPrenda(){
+	  return this.tipoPrenda;
 	}
 }
 
-public class Tarjeta implements MedioDePago {
-	private int cuotas;
-	private float coeficiente;
-		
-	public int calcularPrecioTotal(int precio){
-		return precio + (precio * 0.01 + cuotas * coeficiente);
+enum TipoPrenda {
+	SACO,
+	PANTALON,
+	CAMISA
+}
+
+// PATRON STRATEGY -> LA PRENDA DELEGA PARTE DE SU COMPORTAMIENTO EN TRES OBJETOS POLIMORFICOS QUE COMPARTEN UNA INTERFAZ. CADA OBJETO (O CLASE) SE LLAMAN ESTRATEGIAS
+// 					  QUE SE PUEDEN INTERCAMBIAR DINAMICAMENTE.
+//                    EL CAMBIO DE ESTRATEGIA ES SIEMPRE ARBITARIO (NO HAY ORDEN). EJ: SI UNA PRENDA EMPIEZA SIENDO NUEVA, PUEDE CAMBIAR A PROMOCION O LIQUIDACION.
+//					  LA DECISION DEL CAMBIO ES DE UN OBJETO EXTERNO. NO LO DECIDE NI LA ESTRATEGIA NI EL OBJETO QUE TIENE LA ESTRATEGIA.
+
+interface EstadoPrenda {
+	Double precioFinal(Integer precioOriginal);
+}
+
+class Liquidacion implements EstadoPrenda {
+	@Override
+	public Double precioFinal(Integer precioOriginal) {
+	  return (precioOriginal * 0.50);
+	}
+}
+
+class Nueva implements EstadoPrenda {
+	@Override
+	public Double precioFinal(Integer precioOriginal) {
+	  return Double.valueOf(precioOriginal);
+	}
+}
+
+class Promocion implements EstadoPrenda {
+	Integer descuento;
+  
+	public Promocion(Integer descuento){
+	  this.descuento = descuento;
+	}
+
+	@Override
+	public Double precioFinal(Integer precioOriginal) {
+	  return Double.valueOf((precioOriginal - this.descuento));
 	}
 }
--- queMePongo/queMePongo.java
+++ queMePongo/queMePongo.java
@@ -1,47 +1,74 @@
+import static java.util.Objects.requireNonNull;
+
 class Borrador{
-    TipoPrenda tipoPrenda;
-    Material material;
-    Color colorPrincipal;
-    Color colorSecundario;
-    Trama trama = Trama.LISA;
+    private TipoPrenda tipoPrenda;
+    private Material material;
+    private Color colorPrincipal;
+    private Color colorSecundario;
+    private Trama trama = Trama.LISA;
 
-    Borrador(TipoPrenda tipoPrenda){
-
-        // VALIDAR QUE NO SEA NULO
-
+    public Borrador setTipoPrenda(TipoPrenda tipoPrenda){
         this.tipoPrenda = tipoPrenda;
+        return this;
     }
 
-    public void especificarColorPrincipal(Color color){
-        this.colorPrincipal = color;
-    }
-
-    public void especificarColorSecundario(Color color){
-        this.colorSecundario = color;
-    }
-
-    public void especificarMaterial(Material material){
+    public Borrador setMaterial(Material material){
         this.material = material;
+        return this;
     }
 
-    public void especificarTrama(Trama trama){
+    public Borrador setColorPrincipal(Color color){
+        this.colorPrincipal = color;
+        return this;
+    }
+
+    public Borrador setColorSecundario(Color color){
+        this.colorSecundario = color;
+        return this;
+    }
+
+    public Borrador setTrama(Trama trama){
         this.trama = trama;
+        return this;
     }
 
     public Prenda crearPrenda(){
 
         // VALIDAR QUE NINGUN ATRIBUTO SEA NULL (menos color secundario), CASO CONTARIO, LANZAR EXCEPCION
+        requireNonNull(tipoPrenda, "Debe especificar el tipo de prenda");
+        requireNonNull(material, "Debe especificar el material");
+        requireNonNull(colorPrincipal, "Debe especificar el color principal");
+
+        // OTRA MANERA DE HACERLO ES CREAR UNA FUNCION validarParametros(tipoPrenda, material, colorPrincipal) (*)
 
         return new Prenda(tipoPrenda, material, colorPrincipal, colorSecundario, trama);
     }
+
+// (*) private void validarParametros(TipoPrenda tipoPrenda, Material material, Color colorPrincipal){
+//          if(tipoPrenda == null){
+//              throw new PrendaInvalidaException("debe especificar el tipo de prenda");
+//           }
+//          if(material == null){
+//              throw new PrendaInvalidaException("debe especificar el material");
+//           }
+//          if(colorPrincipal == null){
+//              throw new PrendaInvalidaException("debe especificar el color principal");
+//           }
+//      }
 }
 
+// public PrendaInvalidaException extends RuntimeException{
+//      public PrendaInvalidaException(String causa){
+//          super("La prenda es invalida porque: " + causa);
+//      }   
+// }
+
 class Prenda{
-    TipoPrenda tipoPrenda;
-    Material material;
-    Color colorPrincipal;
-    Color colorSecundario;
-    Trama trama;
+    private TipoPrenda tipoPrenda;
+    private Material material;
+    private Color colorPrincipal;
+    private Color colorSecundario;
+    private Trama trama;
     
     Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal, Color colorSecundario, Trama trama){
         this.tipoPrenda = tipoPrenda;
@@ -51,7 +78,7 @@ class Prenda{
         this.trama = trama;
     }
 
-    Categoria categoria(){
+    public Categoria categoria(){
         return tipoPrenda.getCategoria();
     }
 }
@@ -61,32 +88,33 @@ enum Material{
     JEAN,
     PLASTICO,
     LAYCRA,
-    CUERO
+    CUERO;
 }
+
 class Color{
-    int rojo, azul, verde;
+    private int rojo, azul, verde;
 
     Color(int rojo, int azul, int verde){
-        this.rojo = rojo;
-        this.azul = azul;
-        this.verde = verde;
+        this.rojo = requireNonNull(rojo, "Debe especificar la intensidad del color rojo");
+        this.azul = requireNonNull(azul, "Debe especificar la intensidad del color azul");
+        this.verde = requireNonNull(verde, "Debe especificar la intensidad del color verde");
     }
 }
 
-class TipoPrenda{
+enum TipoPrenda{
+    ZAPATOS(Categoria.CALZADO),
+    CAMISA_MANGAS_CORTAS(Categoria.PARTE_SUPERIOR),
+    PANTALON(Categoria.PARTE_INFERIOR);
 
-    Categoria categoria;
+    private Categoria categoria;
 
-    TipoPrenda(Categoria categoria){
-
-        // VALIDAR QUE LA CATEGORIA ES VALIDA PARA EL TIPO DE PRENDA, CASO CONTARIO, LANZAR EXCEPCION
-
+    private TipoPrenda(Categoria categoria){
         this.categoria = categoria;
     }
 
     public Categoria getCategoria(){
         return categoria;
-    }
+    } 
 }
 
 enum Categoria{
